c92f3e77a50945ba0fd44768b4864bc276328779
Alfredo Tochon <artochq@gmail.com>
2023-10-05 16:12:20 -0500

    Initial Commit

---
+++ ApexPredator/ApexPredatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApexPredatorApp.swift
+//  ApexPredator
+//
+//  Created by Alfredo Tochon on 10/5/23.
+//
+
+import SwiftUI
+
+@main
+struct ApexPredatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ApexPredator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ApexPredator
+//
+//  Created by Alfredo Tochon on 10/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
